718d518b2985868ce3f0d373128d26815fcc94fe
rickeyswenson <105407561+rickeyswenson@users.noreply.github.com>
2022-09-10 19:32:39 -0700

    added complete button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
-import { useState } from 'react'
+import "./App.css";
+import { useState } from "react";
+import { Task } from "./Task";
 
 function App() {
   const [todoList, setTodoList] = useState([]);
-  const [newTask, setNewTask] = useState(""); 
+  const [newTask, setNewTask] = useState("");
 
   const handleChange = (event) => {
     setNewTask(event.target.value);
@@ -11,31 +12,48 @@ function App() {
 
   const addTask = () => {
     const task = {
-      id: todoList.length === 0 ? 1 :  todoList[todoList.length - 1].id + 1, 
-      taskName: newTask
-    }
-    setTodoList([...todoList, task]);  
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: newTask,
+      completed: false,
+    };
+    setTodoList(task.taskName !== "" ? [...todoList, task] : todoList);
   };
 
   const deleteTask = (id) => {
     setTodoList(todoList.filter((task) => task.id !== id));
   };
 
+  const completeTask = (id) => {
+    setTodoList(
+      todoList.map((task) => {
+        if (task.id === id) {
+          return { ...task, completed: true };
+        } else {
+          return task;
+        }
+      })
+    );
+  };
+
   return (
     <div className="App">
       <div className="addTask">
-        <input onChange={handleChange}/>
+        <input onChange={handleChange} />
         <button onClick={addTask}> Add Task</button>
       </div>
-      <div className="List"></div>
+      <div className="list">
         {todoList.map((task) => {
           return (
-           <div>
-             <h1>{task.taskName}</h1>
-              <button onClick={ () => deleteTask(task.id)}> X </button>
-           </div>
+            <Task
+              taskName={task.taskName}
+              id={task.id}
+              completed={task.completed}
+              deleteTask={deleteTask}
+              completeTask={completeTask}
+            />
           );
         })}
+      </div>
     </div>
   );
 }
